668472589b81006638da11aea2ee2e24ca29b98f
Lewis Kimber <lewis.kimber@caplin.com>
2022-10-26 18:30:25 +0100

    Update lua/user/lsp.lua to 0.8 API.

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -39,28 +39,33 @@ local lsp_flags = {
   debounce_text_changes = 150,
 }
 
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 -- configure settings: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 require('lspconfig')['tsserver'].setup {
   on_attach = function(client, bufnr)
-    client.resolved_capabilities.document_formatting = false
+    client.server_capabilities.document_formatting = false
+    client.server_capabilities.documentFormattingProvider = false
     on_attach(client, bufnr)
   end,
-
   flags = lsp_flags,
+  capabilities = capabilities,
 }
 
 require('lspconfig')['eslint'].setup {
   on_attach = on_attach,
   flags = lsp_flags,
+  capabilities = capabilities,
 }
 
 require('lspconfig')['sumneko_lua'].setup {
   on_attach = function(client, bufnr)
-    client.resolved_capabilities.document_formatting = false
+    client.server_capabilities.document_formatting = false
+    client.server_capabilities.documentFormattingProvider = false
     on_attach(client, bufnr)
   end,
-
   flags = lsp_flags,
+  capabilities = capabilities,
   settings = {
     Lua = {
       diagnostics = {
